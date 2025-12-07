5a604e30bfd1077d13cbfc0dce982d1317d431a5
Mel <melissapixell@gmail.com>
2025-12-07 21:13:00 +0300

    Adapt administration

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+
+# добавляем удобство
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status'] # показываем нужные поля в списке
+    list_filter = ['status', 'created', 'publish'] # атрибут фильтров
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)} # автоматический путь
+    raw_id_fields = ['author'] # делаем поиск по автору, а не ищем по списку
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
