d1b8c57f780cf10cd4cc65630bb50c84678306c1
Mel <melissapixell@gmail.com>
2025-12-08 12:13:30 +0300

    Creating a Pots View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request): # для этого представления используем 1 обьект. Этот обьект необходим для всех функций-представлений
+    posts = Post.published.all() # используем менеджер
+    return render(request,
+        'blog/post/list.html', # путь к шаблону
+        {'posts': posts}) # контекстные переменные
