7e2dc9eb2a63f9413a8cc935fe5e6f50f67bd339
Mel <melissapixell@gmail.com>
2025-12-09 11:31:27 +0300

    pagination in the view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
-def post_list(request): # для этого представления используем 1 обьект. Этот обьект необходим для всех функций-представлений
+
+def post_list(request):
+    # получаем все опубликованные посты
     posts = Post.published.all() # используем менеджер
+
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1) # извлекаем HTTP GET-параметр page. При отсуствии - значение 1.
+    posts = paginator.page(page_number) # получаем обьект с методами
+
     return render(request,
         'blog/post/list.html', # путь к шаблону
-        {'posts': posts}) # контекстные переменные
+        {'posts': posts}) # подсставляем контекст
 
 
 # создаем представление о подробности поста
