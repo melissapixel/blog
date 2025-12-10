0d72b88b3f6312051649dc19bedb71cad257b3db
Mel <melissapixell@gmail.com>
2025-12-10 11:25:14 +0300

    create class No error handling

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog' # именное пространство приложения
 # когда пользователь заходит на какой-то url, то вызови его представление
 urlpatterns = [
     # представления поста
-    path('', views.post_list, name='post_list'),  # → /blog/
+    path('', views.PostListView.as_view(), name='post_list'),  # → /blog/
     
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
         views.post_detail, 
