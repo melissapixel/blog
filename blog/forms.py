4600410f204b54a57fa383f486a319449a96983c
Mel <melissapixell@gmail.com>
2025-12-11 20:14:52 +0300

    Creating Shapes from Models

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,18 @@
 # Определение формы
 from django import forms
 
+from .models import Comment
+
 # новый класс наследуется от forms.Form (не привязан к модели)
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
-    email = forms.EmailField() # автоматически проверяет, похоже ли на емейл
+    email = forms.EmailField()                      # проверяет, похоже ли на емейл
     to = forms.EmailField()
-    comments = forms.CharField(required=False, # необязательно для заполнения
-                            widget=forms.Textarea) # указывает, как отображать поле
+    comments = forms.CharField(required=False,      # необязательно для заполнения
+                            widget=forms.Textarea)  # указывает, как отображать поле
+    
+
+class CommentForm(forms.ModelForm):                 # форма Django, привязанная к модели.
+    class Meta:                                     # инструкция для Django, что делать
+        model = Comment                             # форма работает с моделью Comment
+        fields = ['name', 'email', 'body']          # Из всех полей модели покажи в форме ТОЛЬКО эти три
