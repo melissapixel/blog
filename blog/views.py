b12a8c2acbe2d11358007c9d1a60d91cb3fbddf1
Mel <melissapixell@gmail.com>
2026-01-17 15:25:04 +0300

    feat(found-post): Trigram Similarity

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from django.views.generic import ListView
 from django.contrib.postgres.search import SearchVector, \
-                                                        SearchQuery, SearchRank
+                                            SearchQuery, SearchRank, \
+                                            TrigramSimilarity
 from .forms import EmailPostForm, CommentForm,  SearchForm # выгружаем нашу форму
 from django.core.mail import send_mail # функция, которая отправляет email через SMTP-сервер
 from django.views.decorators.http import require_POST # декоратор.
@@ -146,13 +147,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A', config='english') + \
-                            SearchVector('body', weight='B', config='english')
-            search_query = SearchQuery(query, config='english')
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')     # Релевантная сортировка. + отсеять нерелевантные результаты
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')     # Релевантная сортировка. + отсеять нерелевантные результаты
 
     # ← ВЫНОСИМ render() ЗА ПРЕДЕЛЫ УСЛОВИЯ!
     return render(request,
