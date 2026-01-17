b4f1aaef0dd74e501950705a7f51270a84d86272
Mel <melissapixell@gmail.com>
2026-01-17 14:04:56 +0300

    feat: full-text search

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+                                                        SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm,  SearchForm # выгружаем нашу форму
 from django.core.mail import send_mail # функция, которая отправляет email через SMTP-сервер
 from django.views.decorators.http import require_POST # декоратор.
@@ -145,9 +146,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')     # Релевантная сортировка
 
     # ← ВЫНОСИМ render() ЗА ПРЕДЕЛЫ УСЛОВИЯ!
     return render(request,
