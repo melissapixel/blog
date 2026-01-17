8a4b579ed60db75b1d2adbf4ee4872e55ab6cbab
Mel <melissapixell@gmail.com>
2026-01-17 14:50:01 +0300

    feat(found-post): add config

--- blog/views.py
+++ blog/views.py
@@ -146,8 +146,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
+            search_vector = SearchVector('title', 'body', config='english')     # Этот конфиг и так по умолчанию
+            search_query = SearchQuery(query, config='english')
             results = Post.published.annotate(
                 search=search_vector,
                 rank=SearchRank(search_vector, search_query)
